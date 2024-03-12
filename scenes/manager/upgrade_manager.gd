3f9d351c0d91f9b489bd70067d4c7bd9b193d809
Rafael Rios Saavedra <rafael.rios.saavedra@gmail.com>
2024-03-12 20:11:17 +0100

    Lesson 23- Enabling Upgrade Selection (Allow the user to select the upgrade card and apply its effects)

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -19,6 +19,7 @@ func on_level_up(_current_level: int):
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
 	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 
 func apply_upgrade(upgrade: AbilityUpgrade):
 	var has_upgrade = current_upgrades.has(upgrade.id)
@@ -29,3 +30,8 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 		}
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
+	print(current_upgrades)
+
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+signal upgrade_selected(upgrade: AbilityUpgrade)
+
 @export var upgrade_card_scene: PackedScene
 @onready var card_container: HBoxContainer = $%CardContainer
 
@@ -9,6 +11,12 @@ func _ready():
 	
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
 	for upgrade in upgrades:
-		var card_instace = upgrade_card_scene.instantiate()
-		card_container.add_child(card_instace)
-		card_instace.set_ability_upgrade(upgrade)
+		var card_instance = upgrade_card_scene.instantiate()
+		card_container.add_child(card_instance)
+		card_instance.set_ability_upgrade(upgrade)
+		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	upgrade_selected.emit(upgrade)
+	get_tree().paused = false
+	queue_free()
