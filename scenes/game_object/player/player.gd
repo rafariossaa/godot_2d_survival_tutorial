bcba40ef4b01c91fdbac1305acf546ca4c311bee
Rafael Rios Saavedra <rafael.rios.saavedra@gmail.com>
2024-03-13 22:00:33 +0100

    Lesson 26- Adding Player Health

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -3,11 +3,15 @@ extends CharacterBody2D
 const MAX_SPEED = 125
 const ACCELERATION_SMOOTHING = 25
 
+@onready var damage_interval_timer = $DamageIntervalTimer
 
-# Called when the node enters the scene tree for the first time.
+var number_colliding_bodies = 0
+
 func _ready():
-	pass # Replace with function body.
-
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_entered)
+	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -24,3 +28,22 @@ func get_movement_vector():
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
 	return Vector2(x_movement,y_movement)
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+	$HealthComponent.damage(1)
+	damage_interval_timer.start()
+	print($HealthComponent.current_health)
+
+func on_body_entered(other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+
+func on_body_exited(other_body: Node2D):
+	number_colliding_bodies -= 1
+
+
+func on_damage_interval_timer_timeout():
+	check_deal_damage()
