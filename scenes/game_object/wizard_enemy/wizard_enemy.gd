d32989724e26cb906c6638aabfd19b99fc178c06
Rafael Rios Saavedra <rafael.rios.saavedra@gmail.com>
2024-03-31 21:53:03 +0200

    Lesson 41- Animating the Wizard

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -3,13 +3,23 @@ extends CharacterBody2D
 @onready var visuals = $Visuals
 @onready var velocity_component = $VelocityComponent
 
+var is_moving = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	velocity_component.accelerate_to_player()
+
+
+func _process(_delta):
+	if is_moving:
+		velocity_component.accelerate_to_player()
+	else:
+		velocity_component.decelerate()
+		
 	velocity_component.move(self)
 	
 	# Face the sprite in the direction it is moving
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
 		visuals.scale = Vector2(move_sign, 1)
+
+
+func set_is_moving(moving: bool):
+	is_moving = moving
--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -69,5 +69,5 @@ func on_arena_difficulty_increased(arena_difficulty: int):
 	timer.wait_time = base_spawn_time - time_off
 	
 	# at 30 secods we add the wizard
-	if arena_difficulty == 6:
+	if arena_difficulty == 1:
 		enemy_table.add_item(wizard_enemy_scene, 20)
