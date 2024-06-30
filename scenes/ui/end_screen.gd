89528aca29104bead502e1bc7715590a1655c173
Rafael Rios Saavedra <rafael.rios.saavedra@gmail.com>
2024-06-30 19:40:53 +0200

    Lesson 52- Animating Victory and Defeat Screens

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -1,8 +1,16 @@
 extends CanvasLayer
 
+@onready var panel_container = $%PanelContainer
 
 
 func _ready():
+	panel_container.pivot_offset = panel_container.size / 2
+	var tween = create_tween()
+	# The following line is a hack to set the scale to 0.
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+
 	get_tree().paused = true
 	$%RestartButton.pressed.connect(on_restart_button_pressed)
 	$%QuitButton.pressed.connect(on_quit_button_pressed)
@@ -11,6 +19,7 @@ func _ready():
 func set_defeat():
 	$%TitleLabel.text = "Defat"
 	$%DescriptionLabel.text = "You lost!"
+
 
 func on_restart_button_pressed():
 	get_tree().paused = false
