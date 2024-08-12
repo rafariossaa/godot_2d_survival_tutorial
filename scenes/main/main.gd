7ce88d6493b7132f83b4945d2cdae02536a88e77
Rafael Rios Saavedra <rafael.rios.saavedra@gmail.com>
2024-08-12 20:58:28 +0200

    Lesson 65- Creating a Pause Menu

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -2,8 +2,19 @@ extends Node
 
 @export var end_screen_scene: PackedScene
 
+var pause_menu_scene = preload("res://scenes/ui/pause_menu.tscn")
+
+
 func _ready():
 	$%Player.health_component.died.connect(on_player_died) 
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())
+		# We need to stop the propagation of the unhandled input
+		get_tree().root.set_input_as_handled()
+
 
 
 func on_player_died():
